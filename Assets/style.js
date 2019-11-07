ca4eff8b3af7cd2add6cb0cfe2d440f81e961d40
Andrew Rice <riceandrew8@gmail.com>
2019-11-07 17:20:59 -0600

    working click function and working local storage procedures. Time still not displaying in realtime

--- Assets/style.js
+++ Assets/style.js
@@ -1,11 +1,28 @@
+//Click function is opperational
+$('.hours').on('click', function (event) {
+    event.preventDefault();
+    alert("Schedule Updated!");
+    var inputid = $(this).attr("data-input");
+    var value = document.getElementById(inputid).value;
+    localStorage.setItem(inputid, value);
+})
+
+
+//retrieves items stored in local storage and displays them on the screen
+document.getElementById('input9').value = localStorage.getItem('input9')
+document.getElementById('input10').value = localStorage.getItem('input10')
+document.getElementById('input11').value = localStorage.getItem('input11')
+document.getElementById('input12').value = localStorage.getItem('input12')
+document.getElementById('input1').value = localStorage.getItem('input1')
+document.getElementById('input2').value = localStorage.getItem('input2')
+document.getElementById('input3').value = localStorage.getItem('input3')
+document.getElementById('input4').value = localStorage.getItem('input4')
+document.getElementById('input5').value = localStorage.getItem('input5')
+
+
+
+
 //Displays time on screen(not in realtime yet)
 let now = moment();
 console.log(now.format("YYYY MM DD - hh:mm:ss a"));
-
-displaytime.textContent = now
-
-//Displays alert when save button is clicked
-$('#b9').click(function(){
-    alert("Event Scheduled!")
-
-})
+displaytime.textContent = now;
